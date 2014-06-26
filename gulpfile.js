fa6947f120fef288455771c363daa80da9fe2830
John Otander <johnotander@gmail.com>
2014-06-25 18:20:55 -0600

    Add gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass   = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var using  = require('gulp-using');
+var bower  = require('gulp-bower');
+var prefix = require('gulp-autoprefixer');
+var cssmin = require('gulp-minify-css');
+
+gulp.task('default', []);
