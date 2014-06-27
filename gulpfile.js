fe300cf6656e54ac647678d42ea9e37952462f59
John Otander <johnotander@gmail.com>
2014-06-27 15:16:26 -0600

    Add gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,44 @@
 var gulp = require('gulp');
 
-var jshint = require('gulp-jshint');
-var sass   = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var using  = require('gulp-using');
-var bower  = require('gulp-bower');
-var prefix = require('gulp-autoprefixer');
-var cssmin = require('gulp-minify-css');
+var jshint  = require('gulp-jshint');
+var sass    = require('gulp-sass');
+var concat  = require('gulp-concat');
+var uglify  = require('gulp-uglify');
+var rename  = require('gulp-rename');
+var prefix  = require('gulp-autoprefixer');
+var csslint = require('gulp-csslint');
+var cssmin  = require('gulp-minify-css');
 
-gulp.task('default', []);
+gulp.task('scss', function() {
+  gulp.src('scss/c.scss')
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(cssmin())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('csslint', function() {
+  gulp.src('css/c.css')
+    .pipe(csslint({
+      'compatible-vendor-prefixes': false,
+      'box-sizing': false,
+      'important': false,
+      'known-properties': false
+    }))
+    .pipe(csslint.reporter());
+});
+
+gulp.task('js', function() {
+  gulp.src('js/src/*.js')
+    .pipe(uglify())
+    .pipe(concat('j.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('jslint', function() {
+  gulp.src('./js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default', ['scss', 'csslint', 'js', 'jslint']);
